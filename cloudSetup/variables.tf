6af17aa76d3419feed3785fa850fb43a3f322f41
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-03-23 14:31:43 +0100

    Update project short tag

--- cloudSetup/variables.tf
+++ cloudSetup/variables.tf
@@ -6,7 +6,7 @@ variable "deputy" {
 
 # Use a short project tag. Long tags will cause the exasol cluster creation to fail (see https://github.com/exasol/cloudformation-aws-exasol/issues/3)
 variable "project" {
-  default = "VS-ABS"
+  default = "VSABS"
 }
 
 variable "additional_tags" {
