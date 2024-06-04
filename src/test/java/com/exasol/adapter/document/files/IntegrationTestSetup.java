40350a801fc419a21c6a83d3954e95b6644e747b
Pieterjan Spoelders <pj.spoelders@gmail.com>
2024-06-04 04:15:25 -0400

    Security/fix CVE 2024 36114 (#57)
    * Security update - fix for CVE-2024-36114

--- src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
+++ src/test/java/com/exasol/adapter/document/files/IntegrationTestSetup.java
@@ -26,7 +26,7 @@ import com.exasol.udfdebugging.UdfTestSetup;
 import jakarta.json.*;
 
 public class IntegrationTestSetup implements AutoCloseable {
-    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-8.0.3-azure-blob-storage-2.0.4.jar";
+    private static final String ADAPTER_JAR = "document-files-virtual-schema-dist-8.0.4-azure-blob-storage-2.0.5.jar";
     private final ExasolTestSetup exasolTestSetup;
     private final Connection exasolConnection;
     private final Statement exasolStatement;
